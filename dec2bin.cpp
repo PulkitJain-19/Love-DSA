a7b35e537f219d4fa6d0b4a893f86afc1560b37b
cipher <pulkit.jain19062002@gmail.com>
2022-11-13 22:03:38 +0530

    first commit

--- dec2bin.cpp
+++ dec2bin.cpp
@@ -2,18 +2,40 @@
 #include<math.h>
 using namespace std;
 
-int main(){
+/*int main(){
     int n;
     cin>>n; 
     
     int ans=0;
     int i=0;
     while(n!=0){
-        int bit = n&1;
-        ans =(bit*pow(10,i)) + ans; 
+        
+        int bit = n & 1;
+        ans =(bit*pow(10,i)) + ans ; 
          n = n >> 1;
         i++;
     }
     cout<<"it's Binary is "<<ans<<endl;
     return 0;
+}*/
+
+// for negative integer
+
+int main(void){
+long long int n;
+cin>>n;
+unsigned long long int i=0, ans=0;
+if (n<0){
+    n=pow(2,16) +n;
+
+}
+cout<<n<<endl;
+while(n){
+    int lastBit = n&1;
+    ans = (pow(10,i)* lastBit)+ans;
+    n=n>>1;
+    i++;
+}
+cout<<ans<<endl;
+return 0;
 }
